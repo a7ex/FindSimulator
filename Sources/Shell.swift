c39a25f6366d25861c22e8c41c4f1bd08e8beaa1
Alex da Franca <adf@starfinanz.de>
2024-08-02 13:53:45 +0200

    Add regex pattern support Add tests

---
+++ Sources/Shell.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Alex da Franca on 02.08.24.
+//
+
+import Foundation
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,12 @@
+//
+//  LinuxMain.swift
+//
+//  Created by Alex da Franca on 26.12.21.
+//
+
+import XcresultparserTests
+import XCTest
+
+var tests = [XCTestCaseEntry]()
+tests += XcresultparserTests.allTests()
+XCTMain(tests)
